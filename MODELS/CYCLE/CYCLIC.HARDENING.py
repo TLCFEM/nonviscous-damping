28720e9eec0af9b823a4cbda7279d346017ff4bc
Theodore Chang <tlcfem@gmail.com>
2026-05-21 16:32:46 +0200

    feat: update cyclic hardening model parameters for improved accuracy and clarity

--- MODELS/CYCLE/CYCLIC.HARDENING.py
+++ MODELS/CYCLE/CYCLIC.HARDENING.py
@@ -9,7 +9,7 @@ import numpy as np
 model = """node 1 0 0
 node 2 1 0
 
-material Bilinear1D 1 100 1 .01 0
+material Elastic1D 1 {freq2}
 
 element T2D2 1 1 2 1 1
 element Mass 2 2 1 1
@@ -20,15 +20,15 @@ fix2 2 2 1 2
 hdf5recorder 1 Node U 2
 hdf5recorder 2 Node RF 2
 
-amplitude Sine 1 {period} 0.02
+amplitude Sine 1 {period} {amplitude}
 
-displacement 1 1 1 1 2
+cload 1 1 1 1 2
 
-step dynamic 1 {period}
+step dynamic 1 {total_time}
 set ini_step_size {time_step}
 set fixed_step_size 1
 
-integrator UDDNewmark 1 .25 .5 -2 0 10 0
+integrator UDDNewmark 1 .25 .5 -20 0 100 0
 
 converger AbsIncreDisp 2 1E-14 10 0
 
@@ -60,20 +60,27 @@ def _read_first_column_h5(path: Path) -> np.ndarray:
     return data if data.ndim == 1 else data[:, 1]
 
 
-def run_cyclic_hardening(period: float):
+def run_cyclic_hardening(freq: float, period: float):
     with tempfile.TemporaryDirectory(prefix="suanpan_") as tmp:
         tmp_dir = Path(tmp)
         inp_file = tmp_dir / "model.sp"
 
         inp_file.write_text(
-            model.format(period=period, time_step=period / 200), encoding="utf-8"
+            model.format(
+                freq2=freq**2,
+                amplitude=1,
+                period=period,
+                total_time=2 * period,
+                time_step=period / 200,
+            ),
+            encoding="utf-8",
         )
 
         subprocess.run(
             ["suanpan", "-np", "-f", str(inp_file)],
             cwd=tmp_dir,
             check=True,
-            capture_output=True,
+            capture_output=False,
             text=True,
         )
 
@@ -86,8 +93,9 @@ def run_cyclic_hardening(period: float):
 if __name__ == "__main__":
     plt.figure(figsize=(6, 3.5))
 
-    for period in [0.1, 1]:
-        disp, force = run_cyclic_hardening(period)
+    for period in [1e-1]:
+        disp, force = run_cyclic_hardening(freq := 1, period)
         plt.plot(disp, force, marker="x", label=f"{period}")
     plt.legend()
+    plt.tight_layout()
     plt.show()
